8f544fe17aa96eda8237224d81365482da7d5529
coconot <coconot01@naver.com>
2016-10-07 16:12:59 +0900

    161007 SLR_Tutor

---
+++ SLR_Tutor/src/js/resources/nls/myTranslations.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define({
+   "root":{
+       "appTitle": "COCONOT Tutor",
+       "menu": {
+           "dashboard": "Dashboard",
+           "lectures": "Lectures",
+           "signup": "Signup",
+           "profile": "Profile"
+       }
+   },
+   ko: 1,
+   fr: 0
+});
